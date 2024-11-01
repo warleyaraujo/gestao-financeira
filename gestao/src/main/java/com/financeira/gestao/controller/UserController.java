332d98ede72b756d153f6ce08bd1a9dc5ff8b8ee
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2024-11-01 13:22:20 -0300

    feat: create first route, hello world

--- gestao/src/main/java/com/financeira/gestao/controller/UserController.java
+++ gestao/src/main/java/com/financeira/gestao/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.financeira.gestao.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
 }
