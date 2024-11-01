799120a365dad980595de513817eb4227ec17ad6
Gabriel B Moura <gabriel.moura2009@hotmail.com>
2024-11-01 13:22:33 -0300

    feat: create table of user

--- gestao/src/main/java/com/financeira/gestao/model/UserModel.java
+++ gestao/src/main/java/com/financeira/gestao/model/UserModel.java
@@ -1,4 +1,79 @@
 package com.financeira.gestao.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "user")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(nullable = false)
+    private String familyName;
+
+    @NotNull
+    @Column(nullable = false)
+    private String email;
+
+    private String tel;
+
+    @NotNull
+    @Column(nullable = false)
+    private String password;
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public @NotNull String getName() {
+        return name;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public @NotNull String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(@NotNull String familyName) {
+        this.familyName = familyName;
+    }
+
+    public @NotNull String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotNull String email) {
+        this.email = email;
+    }
+
+    public @NotNull String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotNull String password) {
+        this.password = password;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
 }
--- gestao/src/main/java/com/financeira/gestao/service/UserService.java
+++ gestao/src/main/java/com/financeira/gestao/service/UserService.java
@@ -1,4 +1,7 @@
 package com.financeira.gestao.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
 }
